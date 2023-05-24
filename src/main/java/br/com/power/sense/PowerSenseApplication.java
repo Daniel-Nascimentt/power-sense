e9b9d6e5d6e3e6eec91bd04f51627f382e59ee72
Daniel-Nasciment <daniel.fecaf@outlook.com>
2023-05-24 13:40:05 -0300

    Feat: Setup Inicial

---
+++ src/main/java/br/com/power/sense/PowerSenseApplication.java
@@ -0,0 +1,13 @@
+package br.com.power.sense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerSenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerSenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/power/sense/PowerSenseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.power.sense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerSenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
